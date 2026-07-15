ae6d29a725ceda97f680a8c9d5b7fa5cf167cba8
Deanolai <3208194348@qq.com>
2026-07-15 10:18:07 +0800

    refactor: improve task 1 query with CONCAT full name and table alias

--- sql/01_active_customer_list.sql
+++ sql/01_active_customer_list.sql
@@ -1,8 +1,12 @@
 --01_active_customer_list
-
+-- v1  [2026-07-15] submitted version
+-- v2  [2026-07-15] refine: CONCAT full name, table alias, code formatting
 
 use sakila;
-select customer.customer_id, customer.first_name, customer.last_name
-from customer
-where customer.active = 1
-order by customer.last_name, customer.first_name;
+
+select
+    c.customer_id,
+    concat(c.first_name, ' ', c.last_name) as full_name
+from customer c
+where c.active = 1
+order by c.last_name, c.first_name;
